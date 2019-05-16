c624a4d09605af72175b19d4ef266d511f2ce2de
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-05-16 15:42:33 +0300

    Initial commit. Spring boot project

---
+++ src/main/java/by/bsac/Runner.java
@@ -0,0 +1,15 @@
+package by.bsac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  Spring boot main class.
+ */
+@SpringBootApplication
+public class Runner  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class);
+    }
+}
